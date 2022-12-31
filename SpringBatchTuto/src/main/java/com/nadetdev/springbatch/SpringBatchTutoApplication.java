66b4fdf76dfbb6922ccaa0f67dfae9bdec1bbe29
PdskDev <prestadesk@gmail.com>
2022-12-31 14:09:18 +0100

    Init commit + First Job demo

---
+++ SpringBatchTuto/src/main/java/com/nadetdev/springbatch/SpringBatchTutoApplication.java
@@ -0,0 +1,17 @@
+package com.nadetdev.springbatch;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableBatchProcessing
+@ComponentScan("com.nadetdev.springbatch.config")
+public class SpringBatchTutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchTutoApplication.class, args);
+	}
+
+}
---
+++ SpringBatchTuto/src/test/java/com/nadetdev/springbatch/SpringBatchTutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadetdev.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchTutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
