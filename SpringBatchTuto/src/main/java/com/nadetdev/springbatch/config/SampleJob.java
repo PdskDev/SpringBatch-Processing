70a32573927c7c11ae87d736187f33744bd68459
PdskDev <prestadesk@gmail.com>
2023-01-01 17:15:36 +0100

    Pass File Resource as JobParameters

--- SpringBatchTuto/src/main/java/com/nadetdev/springbatch/config/SampleJob.java
+++ SpringBatchTuto/src/main/java/com/nadetdev/springbatch/config/SampleJob.java
@@ -15,6 +15,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -25,6 +26,7 @@ import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
@@ -113,20 +115,26 @@ public class SampleJob {
 		return setBuilderFactory.get("First Chunk Step")
 				.<StudentCsv, StudentCsv>chunk(3)
 				//.reader(firstItemReader)
-				.reader(flatFileItemReader())
+				.reader(flatFileItemReader(null))
 				//.processor(firstItemProcessor)
 				.writer(firstItemWriter)
 				.build();
 	}
 	
-	
-	
-	public FlatFileItemReader<StudentCsv> flatFileItemReader(){
+	@StepScope
+	@Bean
+	public FlatFileItemReader<StudentCsv> flatFileItemReader(
+			@Value("#{jobParameters['inputFile']}") FileSystemResource csvResourceFile){
 		
 		FlatFileItemReader<StudentCsv> flatFileItemReader = new FlatFileItemReader<>();
 		
-		flatFileItemReader.setResource(new FileSystemResource(
-				new File("D:\\Devs\\LocalGitRepository\\SpringBatch-Processing-Udemy\\SpringBatchTuto\\InputFile\\students2.csv")));
+		/*
+		 * flatFileItemReader.setResource(new FileSystemResource( new File(
+		 * "D:\\\\Devs\\\\LocalGitRepository\\\\SpringBatch-Processing-Udemy\\\\SpringBatchTuto\\\\InputFile\\\\students2.cs"
+		 * )));
+		 */
+		
+		flatFileItemReader.setResource(csvResourceFile);
 		
 		flatFileItemReader.setLineMapper(new DefaultLineMapper<StudentCsv>() {
 			{
